5266d020e406f3bc21af897dca0e93eeb763a81c
Skelvir <m4rtin.klein@outlook.de>
2023-02-26 15:29:57 +0100

    Removed Y Z swap

--- keyboards/gmmk/gmmk2/p96/ansi/keymaps/skelvir/keymap.c
+++ keyboards/gmmk/gmmk2/p96/ansi/keymaps/skelvir/keymap.c
@@ -26,7 +26,6 @@
  * Ü mapped to AltGr+Shift+U or AltGr+Shift+Y
  * ü mapped to AltGr+u or AltGr+y
  * ß mapped to AltGr+s
- * Z and Y swapped -> QWERTZ
  * Shift+Backspace mapped to Delete, therefore Ctrl+Shift+Backspace deletes the next word (=Ctrl+Delete)
  *
  *
@@ -62,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_NLCK,  KC_PSLS,  KC_PAST,  KC_PMNS,
   KC_TAB,   DE_AE_OnQ,     KC_W,     KC_E,     KC_R,     KC_T,     DE_UE_OnY,     DE_UE_OnU,     KC_I,     DE_OE_OnO,     DE_OE_OnP,     KC_LBRC,  KC_RBRC,  KC_BSLS,   KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
   MO(_FL),  DE_AE_OnA,     DE_SS_OnS,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,   KC_P4,    KC_P5,    KC_P6,
-  KC_LSFT,  KC_Y,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_P1,    KC_P2,    KC_P3,    KC_PENT,
+  KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_P1,    KC_P2,    KC_P3,    KC_PENT,
   KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,                                 KC_RALT,  MO(_FL),  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT),
 
   /* Keymap _FL: Function Layer
@@ -240,7 +239,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
         }
         else
         {
-          tap_key(KC_Z); // default processing Z -> swapped instead of Y
+          tap_key(KC_Y); // default processing Y
         }
 
         return false; // Skip further key processing
